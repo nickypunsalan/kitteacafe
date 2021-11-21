a6eb20ae276681a66c94a802fbe45f1266bc9b89
nickypuncakes <nicky.puncakes@gmail.com>
2021-11-21 21:44:58 +1100

    Repo restructure

---
+++ src/server/db/index.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(process.env.DB_CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
